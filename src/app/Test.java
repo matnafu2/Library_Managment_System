718b1933f303d51eb02272f87964648c853ca8b6
miki <mikemico30@gmail.com>
2020-06-01 18:31:29 -0400

    fixed the conflict

--- src/app/Test.java
+++
@@ -1,8 +0,0 @@
-package app;
-
-public class Test {
-    public static void main(String[] args) {
-        String name = "65";
-        System.out.println(Math.abs(name.hashCode()));
-    }
-}
--- src/app/TestClass.java
+++
@@ -1,8 +0,0 @@
-package app;
-
-public class TestClass {
-    public static void main(String[] args) {
-        String string = "[firstName=fajjskjkljl, lastName=ksjkjwlkj, email=j, address=slfkjwl, city=jfljl, contactNum=7928742]";
-        System.out.println(string.contains("7"));
-    }
-}
